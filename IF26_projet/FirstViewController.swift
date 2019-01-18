30366901bd2dbb7d41c69749e19ad6b8e00a1c1d
tomherbin <tomherbinth@gmail.com>
2019-01-18 13:36:58 +0100

    début tableview et vdd

--- IF26_projet/FirstViewController.swift
+++ IF26_projet/FirstViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import SQLite
 
 class FirstViewController: UIViewController {
 
+    private var database: Connection!
+    
+    let usersTable = Table("users")
+    let id = Expression<Int>("id")
+    let name = Expression<String>("name")
+    let email = Expression<String>("email")
+    
+    
+    
+    
+    
+    @IBAction func ajouter(_ sender: UIButton) {
+        
+        print("LOl");
+        
+        let createTable = self.usersTable.create { (table) in
+            table.column(self.id, primaryKey: true)
+            table.column(self.name)
+            table.column(self.email, unique: true)
+        }
+        
+        do {
+            try self.database.run(createTable)
+            print("Created Table")
+        } catch {
+            print(error)
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            let documentDirectory = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            let fileUrl = documentDirectory.appendingPathComponent("data_base").appendingPathExtension("sqlite3")
+            let base = try Connection(fileUrl.path)
+            self.database = base;
+
+        }catch{
+            print(error)
+        }
+        
     }
-//test
+
 
 }
 
