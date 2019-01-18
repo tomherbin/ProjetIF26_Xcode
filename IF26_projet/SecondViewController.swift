38cdc76e549a6dfcceed9b6754f484dca5fb84a5
tomherbin <tomherbinth@gmail.com>
2019-01-18 09:37:59 +0100

    Initial Commit

---
+++ IF26_projet/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  IF26_projet
+//
+//  Created by Tom Hache on 18/01/2019.
+//  Copyright © 2019 Tom Herbin TH. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
